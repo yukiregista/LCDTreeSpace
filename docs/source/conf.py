391c500d677c70eab3851fc5e745d7aca6bbddca
Yuki <s.yuki.regista@gmail.com>
2023-01-24 04:28:33 +0900

    Add document pages

---
+++ docs/source/conf.py
@@ -0,0 +1,63 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+
+import os
+import sys
+#sys.path.insert(0, os.path.abspath('../../src/lcdtreespace'))
+sys.path.insert(0, os.path.abspath('../../src'))
+
+
+project = 'lcdtreespace'
+copyright = '2023, Yuki Takazawa'
+author = 'Yuki Takazawa'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+#extensions = [
+#    'sphinx.ext.duration',
+#    'sphinx.ext.doctest',
+#    'sphinx.ext.autodoc',
+#    'sphinx.ext.autosummary',
+#    'sphinx.ext.todo',
+#    'sphinx.ext.viewcode',
+#    'sphinx.ext.napoleon',
+#]
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.todo',
+    'sphinx.ext.napoleon'
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+napoleon_google_docstring = True
+napoleon_numpy_docstring = True
+napoleon_include_init_with_doc = False
+napoleon_include_private_with_doc = False
+napoleon_include_special_with_doc = True
+napoleon_use_admonition_for_examples = False
+napoleon_use_admonition_for_notes = False
+napoleon_use_admonition_for_references = False
+napoleon_use_ivar = False
+napoleon_use_param = True
+napoleon_use_rtype = True
+napoleon_preprocess_types = False
+napoleon_type_aliases = None
+napoleon_attr_annotations = True
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+#html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
---
+++ docs/source/generated/lcdtreespace._mle_1dim.rst
@@ -0,0 +1,31 @@
+﻿lcdtreespace.\_mle\_1dim
+========================
+
+.. automodule:: lcdtreespace._mle_1dim
+
+   
+   
+   
+
+   
+   
+   .. rubric:: Functions
+
+   .. autosummary::
+   
+      lcmle_1dim
+      tree_hull
+      tree_hull_bend
+   
+   
+
+   
+   
+   
+
+   
+   
+   
+
+
+
