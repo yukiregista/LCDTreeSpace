4a7ebc5b6cc02e9852d4571ce49fe7fd78a147f4
Yuki <s.yuki.regista@gmail.com>
2023-01-24 09:30:04 +0900

    testing docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -33,7 +33,7 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.viewcode',
     'sphinx.ext.todo',
-    'sphinx.ext.napoleon'
+    'sphinx.ext.napoleon',
     'sphinx.ext.githubpages'
 ]
 
